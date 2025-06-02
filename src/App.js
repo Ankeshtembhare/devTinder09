60fbc52ecfccbd5eb8670812900b2d14c9d13257
Ankesh <atankesh219@gmail.com>
2025-06-02 22:22:16 +0530

    api level sanitiaztion

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 
  app.post("/signup",async(req,res)=>{
     //creating new instance of the user model
-    
+
     const userdata = new User(req.body);
 
     try{
@@ -22,6 +22,82 @@
   
 
  })
+
+ app.get("/user",async(req,res)=>{
+    const useremaill = req.body.emailID;
+
+    try{
+        const users = await User.find({emailID:useremaill});
+
+        if(users.length === 0){
+             res.status(400).send("something went wrong");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+ });
+
+ app.get("/feed",async(req,res)=>{
+
+    try{
+        const users = await User.find({});
+
+        if(users.length === 0){
+             res.status(400).send("something went wrong");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+ });
+
+  app.delete("/user",async(req,res)=>{
+    const userid = req.body.userID;
+
+    try{
+        const users = await User.findByIdAndDelete(userid);
+
+       res.send("deleted succesfully");
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+ });
+
+ app.patch("/user/:userID",async(req,res)=>{
+    const userid = req.params?.userID;
+    const data = req.body;
+     try{
+        const allowed_updates = ["photoUrl","about","skills","age"];
+        const is_update = Object.keys(data).every((k)=>
+        allowed_updates.includes(k));
+
+        if(!is_update){
+            throw new Error("update is not allowed");
+        }
+        if(data?.skills.length >10){
+             throw new Error("more than 10 skills is not allowed");
+        }
+        const users = await User.findByIdAndUpdate({_id:userid},data,{
+            returnDocument:"after",
+            runValidators:true,
+        });
+        console.log(users);
+
+       res.send("updated succesfully");
+    }
+    catch(err){
+        res.status(400).send("UPDATE FAILED"+err.message);
+    }
+
+ });
 
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,60 @@
 
 const mongoose = require("mongoose");
 
-const userschema = new mongoose.Schema({
+const userschema = new mongoose.Schema(
+    {
 
         firstName:{
             type:String,
+            required:true,
+            minLength:4,
+            maxLength:50,
         },
         lastName:{
             type: String,
         },
         emailID:{
             type: String,
+            unique:true,
+            lowercase:true,
+            minLength:4,
+            trim:true,
+            maxLength:50,
         },
          password:{
             type:String,
+            required:true,
+
         },
          age:{
             type:Number,
+            min:18,
+            max:100,
         },
          gender:{
             type: String,
+            validate(value){
+                if(!["male","female","others"].includes(value)){
+                    throw new Error("gender is not defined");    
+                }
+            }
         },
+        photoUrl:{
+            type:String,
+        },
+        about:{
+            type:String,
+            default:"this is about for user",
+        },
+        skills:{
+            type:[String],
 
-})
+        },
+    },
+    {   
+        timestamps:true,
+    }
+)
 
 
 module.exports = mongoose.model("User",userschema);
